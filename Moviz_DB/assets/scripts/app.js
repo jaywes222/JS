ccd6e6be0e0cc3c35759b6c7989faafb792deccd
Jay Wes <wesleyjay202@gmail.com>
2024-05-29 14:34:38 -0700

    Controlling the Backdrop

--- Moviz_DB/assets/scripts/app.js
+++ Moviz_DB/assets/scripts/app.js
@@ -5,8 +5,29 @@ const addMovieModal = document.getElementById('add-modal');
 // const startAddMovieButton1 = document.querySelector('header').lastElementChild;
 const startAddMovieButton = document.querySelector('header button');
 
+const backdrop = document.getElementById('backdrop');
+const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
+
+//Opens the Modal button
 const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
 };
 
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+}
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
+}
+
+const cancelAddMovie = () => {
+    toggleMovieModal();
+}
+
+
+
 startAddMovieButton.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelAddMovieButton.addEventListener('click', cancelAddMovie)
