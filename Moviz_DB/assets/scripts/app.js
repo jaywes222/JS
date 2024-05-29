344f91e380a7593dd8a8b4555a6c621ec5aab30c
Jay Wes <wesleyjay202@gmail.com>
2024-05-29 15:19:12 -0700

    Rendering Movie Items on the Screen

--- Moviz_DB/assets/scripts/app.js
+++ Moviz_DB/assets/scripts/app.js
@@ -9,6 +9,7 @@ const backdrop = document.getElementById('backdrop');
 const cancelAddMovieButton = addMovieModal.querySelector('.btn--passive');
 const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling;
 const userInputs = addMovieModal.querySelectorAll('input');
+const entryTextSection = document.getElementById('entry-text');
 
 const movies = [];
 
@@ -39,6 +40,30 @@ const cancelAddMovieHandler = () => {
     clearMovieInputs();
 }
 
+const updateUI = () => {
+    if (movies.length === 0) {
+        entryTextSection.style.display = 'block';
+    } else {
+        entryTextSection.style.display = 'none';
+    }
+};
+
+const renderNewMovieElement = (title, imageUrl, rating) => {
+    const newMovieElement = document.createElement('li');
+    newMovieElement.className = 'movie-element';
+    newMovieElement.innerHTML = `
+    <div class="movie-element__image">
+        <img src="${imageUrl}" alt="${title}">
+    </div>
+    <div class="movie_element__info">
+        <h2>${title}</h2>
+        <p>${rating}/5 stars</p>
+    </div>
+    `;
+    const listRoot = document.getElementById('movie-list');
+    listRoot.append(newMovieElement);
+};
+
 const addMovieHandler = () => {
     const titleValue = userInputs[0].value;
     const imageUrlValue = userInputs[1].value;
@@ -63,6 +88,8 @@ const addMovieHandler = () => {
     console.log(movies);
     toggleMovieModalHandler();
     clearMovieInputs();
+    renderNewMovieElement(newMovie.title, newMovie.image, newMovie.rating);
+    updateUI();
 }
 
 startAddMovieButton.addEventListener('click', toggleMovieModalHandler);
